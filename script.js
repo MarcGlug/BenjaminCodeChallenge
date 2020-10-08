3ede6a56072775a7b1bdb0de278651109cb75465
MarcGlug <marc.boyon@hotmail.fr>
2020-10-08 14:46:41 +0200

    Circles responsive

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 window.onload = function(){
 
-document.onmousemove = mousePosition;
 backCircle = document.getElementById("backCircle");
 frontCircle = document.getElementById("frontCircle");
 imgBenj = document.getElementById("imgBenj");
+imgPart = document.getElementsByClassName("imgPart");
+    
+//On lance une fois le circle resize pour que les cercles aient tout de suite
+// la bonne taille
+circleResize();
+
+//EVENTS
+document.onmousemove = mousePosition;
+window.onresize = circleResize;
+
 
 function mousePosition(e){
     const w = window.innerWidth;
@@ -13,5 +22,15 @@ function mousePosition(e){
     backCircle.style.transform = "translate("+ xPer * 10 + "%)";
     frontCircle.style.transform = "translate("+ xPer * -250 + "%)";
     imgBenj.style.transform = "translate("+ xPer * (-5) + "%)";
+}
+
+function circleResize(){
+    w = imgPart[0].offsetWidth;
+    h = imgPart[0].offsetHeight;
+    backCircle.style.width = w*0.8 + "px";
+    backCircle.style.height = w *0.8 + "px";
+    frontCircle.style.width = w*0.1 + "px";
+    frontCircle.style.height = w *0.1 + "px";
+    
 }
 }
